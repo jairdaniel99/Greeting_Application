1b5c0df9d4a5145b614c3935910197c74ece6212
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-05 18:52:43 -0600

    updated root components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// a component is like an HTML element with its own template, styles and logic
+// Use the @Component decorator to define a component and its metadata
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // CSS selector we use to refer to this component in an html file
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html', // path to html template file
+  styleUrl: './app.component.css', // path to css file
 })
 export class AppComponent {
-  title = 'Greeting_Application';
+  title = 'Angular';
+  studentName = 'Jair';
 }
